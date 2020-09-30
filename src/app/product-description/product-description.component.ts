c0e195ca3204c7580666e46685257ca0cacd3eb3
alston332 <alston332@gmail.com>
2020-09-30 22:03:36 +0000

    Completed module

---
+++ src/app/product-description/product-description.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Response } from '@angular/http';
+import {ProductService} from '../Product.Service';
+
+@Component({
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
+})
+export class ProductDescriptionComponent implements OnInit {
+
+  albumInfo;
+
+  constructor(private _productService: ProductService ) { }
+
+  ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response =>
+     this.albumInfo = response );
+  }
+
+}
