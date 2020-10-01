e943041dca909c4d16ce8627c2c5e23ad71d8def
alston332 <alston332@gmail.com>
2020-10-01 10:20:37 +0000

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/Observable/'
+import {Album} from './album';
 
 
 @Injectable()
