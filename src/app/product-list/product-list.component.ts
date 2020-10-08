de132dc52452e1e18b043d42f9c749e1843aafd8
alston332 <alston332@gmail.com>
2020-10-08 17:35:47 +0000

    Completed module

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../product';
+import {ProductServices} from '../product.service';
 
 @Component({
   selector: 'app-product-list',
